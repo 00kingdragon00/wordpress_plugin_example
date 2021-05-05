61148f9ea035f4ec572797e3270618f0eb0fa413
king-dragon <utkarshchauhan17@outlook.com>
2021-05-05 21:59:15 +0530

    register and enqueue recipe rating style and js

---
+++ includes/front/enqueue.php
@@ -0,0 +1,26 @@
+<?php
+
+function r_enqueue_scripts()
+{
+    $ver            = JU_DEV_MODE ? time() : false;
+
+    wp_register_style('r_rateit', plugins_url('/assets/rateit/rateit.css', RECIPE_PLUGIN_URL), [], $ver);
+    wp_enqueue_style('r_rateit');
+
+    wp_register_script(
+        'r_rateit',
+        plugins_url('/assets/rateit/jquery.rateit.min.js', RECIPE_PLUGIN_URL),
+        ['jquery'],
+        $ver,
+        true
+    );
+
+    wp_register_script('r_main', plugins_url('/assets/js/main.js', RECIPE_PLUGIN_URL), ['jquery'], $ver, true);
+
+    wp_localize_script('r_main', 'recipe_obj', [
+        'ajax_url'      =>  admin_url('admin-ajax.php')
+    ]);
+
+    wp_enqueue_script('r_rateit');
+    wp_enqueue_script('r_main');
+}
