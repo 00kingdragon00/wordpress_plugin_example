1685e2d9efa084e9e7b7365a5ec1447e3c4d76a4
king-dragon <utkarshchauhan17@outlook.com>
2021-05-07 16:18:37 +0530

    render the block

--- blocks/app/recipe-block/index.js
+++ blocks/app/recipe-block/index.js
@@ -19,21 +19,31 @@ registerBlockType("test/recipe", {
   },
   attributes: {
     ingredients: {
+      type: "string",
       source: "text",
+      selector: ".ingredients-ph",
     },
     cooking_time: {
+      type: "string",
       source: "text",
+      selector: ".cooking-time-ph",
     },
     utensils: {
+      type: "string",
       source: "text",
+      selector: ".utensils-ph",
     },
     cooking_experience: {
+      type: "string",
       source: "text",
       default: "Beginner",
+      selector: ".cooking-experience-ph",
     },
     meal_type: {
+      type: "string",
       source: "text",
       default: "Dinner",
+      selector: ".meal-type-ph",
     },
   },
   edit: (props) => {
@@ -103,26 +113,55 @@ registerBlockType("test/recipe", {
       <div className={props.className}>
         <ul class="list-unstyled">
           <li>
-            <strong>{__("Ingredients:", "recipe")}</strong> {props.attributes.ingredients}
+            <strong>{__("Ingredients:", "recipe")}</strong>{" "}
+            <span className="ingredients-ph">{props.attributes.ingredients}</span>
           </li>
           <li>
-            <strong>{__("Cooking Time:", "recipe")} </strong> {props.attributes.cooking_time}
+            <strong>{__("Cooking Time:", "recipe")} </strong>{" "}
+            <span className="cooking-time-ph">{props.attributes.cooking_time}</span>
           </li>
           <li>
-            <strong>{__("Utensils:", "recipe")} </strong> {props.attributes.utensils}
+            <strong>{__("Utensils:", "recipe")} </strong>{" "}
+            <span className="utensils">{props.attributes.utensils}</span>
           </li>
           <li>
-            <strong>{__("Cooking Experience:", "recipe")} </strong>{" "}
-            {props.attributes.cooking_experience}
+            <strong>{__("Cooking Experience:", "recipe")} </strong>
+            <span className="cooking-experience-ph">{props.attributes.cooking_experience}</span>
           </li>
           <li>
-            <strong>{__("Meal Type:", "recipe")} </strong> {props.attributes.meal_type}
+            <strong>{__("Meal Type:", "recipe")} </strong>{" "}
+            <span className="meal-type-ph">{props.attributes.meal_type}</span>
           </li>
         </ul>
       </div>,
     ];
   },
   save: (props) => {
-    return <p>Hello World</p>;
+    return (
+        <div >
+        <ul class="list-unstyled">
+          <li>
+            <strong>{__("Ingredients:", "recipe")}</strong>{" "}
+            <span className="ingredients-ph">{props.attributes.ingredients}</span>
+          </li>
+          <li>
+            <strong>{__("Cooking Time:", "recipe")} </strong>{" "}
+            <span className="cooking-time-ph">{props.attributes.cooking_time}</span>
+          </li>
+          <li>
+            <strong>{__("Utensils:", "recipe")} </strong>{" "}
+            <span className="utensils">{props.attributes.utensils}</span>
+          </li>
+          <li>
+            <strong>{__("Cooking Experience:", "recipe")} </strong>
+            <span className="cooking-experience-ph">{props.attributes.cooking_experience}</span>
+          </li>
+          <li>
+            <strong>{__("Meal Type:", "recipe")} </strong>{" "}
+            <span className="meal-type-ph">{props.attributes.meal_type}</span>
+          </li>
+        </ul>
+      </div>
+    );
   },
 });
