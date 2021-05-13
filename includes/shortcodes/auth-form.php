e74a496ecb56719d4031fa051192a2312dcfcdbb
king-dragon <utkarshchauhan17@outlook.com>
2021-05-13 18:27:50 +0530

    create logic logci

--- includes/shortcodes/auth-form.php
+++ includes/shortcodes/auth-form.php
@@ -2,10 +2,19 @@
 
 function r_recipe_auth_form_shortcode()
 {
+    if(is_user_logged_in()) {
+        // header("Location: /");
+        return '';
+    }
     $formHTML           =   file_get_contents('auth-form-template.php', true);
     $formHTML           =   str_replace(
         'NONCE_FILED_PH',
         wp_nonce_field('recipe_auth', '_wpnonce', true, false),
+        $formHTML
+    );
+    $formHTML           =   str_replace(
+        'SHOW_REG_FORM',
+        (!get_option('user_can_register') ? 'style="display:none"' : ''),
         $formHTML
     );
     return $formHTML;
