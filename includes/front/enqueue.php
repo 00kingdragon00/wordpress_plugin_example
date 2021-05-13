1aa051da39705db29d9dd62393a51a72d95f2ead
king-dragon <utkarshchauhan17@outlook.com>
2021-05-13 18:28:51 +0530

    add auth logic

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -18,7 +18,8 @@ function r_enqueue_scripts()
     wp_register_script('r_main', plugins_url('/assets/js/main.js', RECIPE_PLUGIN_URL), ['jquery'], $ver, true);
 
     wp_localize_script('r_main', 'recipe_obj', [
-        'ajax_url'      =>  admin_url('admin-ajax.php')
+        'ajax_url'      =>  admin_url('admin-ajax.php'),
+        'home_url'      =>  home_url('/')
     ]);
 
     wp_enqueue_script('r_rateit');
--- index.php
+++ index.php
@@ -21,10 +21,13 @@ define('RECIPE_PLUGIN_URL', __FILE__);
 include('includes/activate.php');
 include('includes/init.php');
 include('includes/front/enqueue.php');
+include('includes/front/logout-link.php');
 include('process/save-post.php');
 include('process/rate-recipe.php');
 include('process/filter-content.php');
 include('process/submit-user-recipe.php');
+include('process/create-account.php');
+include('process/login.php');
 include('includes/admin/init.php');
 include('blocks/enqueue.php');
 include(dirname(RECIPE_PLUGIN_URL) . '/includes/widgets.php');
@@ -34,6 +37,7 @@ include('includes/utility.php');
 include('includes/deactivate.php');
 include('includes/shortcodes/creator.php');
 include('includes/shortcodes/auth-form.php');
+include('includes/shortcodes/auth-form-alt.php');
 
 // Hook
 register_activation_hook(__FILE__, 'r_activate_plugin');
@@ -51,7 +55,14 @@ add_action('enqueue_block_editor_assets', 'r_enqueue_block_editor_assets');
 add_action('enqueue_block_assets', 'r_enqueue_block_assets');
 add_action('widgets_init', 'r_widgets_init');
 add_action('r_daily_recipe_hook', 'r_generate_daily_recipe');
+add_action('wp_ajax_nopriv_recipe_create_account', 'recipe_create_account');
+add_action('wp_ajax_nopriv_recipe_user_login', 'recipe_user_login');
+// add_filter('authenticate', 'wp_authenticate_username_password', 20, 3);
+// add_filter('authenticate', 'wp_authenticate_spam_check', 99);
+// add_filter('authenticate', 'r_alt_authenticate', 100, 3);
+add_filter('wp_nav_menu_secondary_items', 'ju_new_nav_menu_items', 999);
 
 // Shortcodes
 add_shortcode('recipe_creator', 'r_recipe_creator_shortcode');
 add_shortcode('recipe_auth_form', 'r_recipe_auth_form_shortcode');
+// add_shortcode('recipe_auth_alt_form', 'r_recipe_auth_alt_form_shortcode');
