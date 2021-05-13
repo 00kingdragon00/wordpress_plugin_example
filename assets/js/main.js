7709e05051f903984701c9b0d73928514781476b
king-dragon <utkarshchauhan17@outlook.com>
2021-05-13 18:27:13 +0530

    add login logic ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,6 @@
 
   $("#recipe-form").on("submit", function (e) {
     e.preventDefault();
-
     $(this).hide();
     $("#recipe-status").html(
       '<div class="alert alert-info ">Please wait! We are submitting your recipe.</div>'
@@ -59,12 +58,40 @@
     $.post(recipe_obj.ajax_url, form).always(function (data) {
       if (data.status == 2) {
         $("#register-status").html('<div class="alert alert-success ">Account created!.</div>');
+        location.href = recipe_obj.home_url;
       } else {
         $("#register-status").html(
           '<div class="alert alert-danger ">Unable to create an account.</div>'
         );
 
         $("#register-form").show();
+      }
+    });
+  });
+
+  $(document).on("submit", "#login-form", function (e) {
+    e.preventDefault();
+
+    $("#login-status").html('<div class="alert alert-info">Please wait while we log you in!</div>');
+    $(this).hide();
+
+    var form = {
+      _wpnonce: $("#_wpnonce").val(),
+      action: "recipe_user_login",
+      username: $("#login-form-username").val(),
+      pass: $("#login-form-password").val(),
+    };
+
+    $.post(recipe_obj.ajax_url, form).always(function (data) {
+      if (data.status == 2) {
+        $("#login-status").html('<div class="alert alert-success ">Success!.</div>');
+        location.href = recipe_obj.home_url;
+      } else {
+        $("#login-status").html(
+          '<div class="alert alert-danger ">Unable to login.</div>'
+        );
+
+        $("#login-form").show();
       }
     });
   });
